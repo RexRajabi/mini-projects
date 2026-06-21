8b7afee1ac591992ebc03303c6c57a45cfadc309
RexRajabi <mohammad.rajabi.ba@gmail.com>
2026-06-22 01:45:07 +0330

    First simple code on postion calculator
    It gets capital, risk percentage, entry and stop.
    Then with a simple function it calculates the lot size.
    And it shows the result by returning a simple dic.

---
+++ position-size-calculator/position-size-calculator.py
@@ -0,0 +1,13 @@
+capital = float(input("Capital: "))
+risk_percent = float(input("Risk %: "))
+entry = float(input("Entry: "))
+stop = float(input("Stop: "))
+
+def postition_calculator(capital, risk_percent, entry, stop):
+    risk_amount = capital * (risk_percent / 100)
+    lot_size = int(risk_amount / (entry - stop))
+    return {
+        "risk amount": risk_amount,
+        "Lot size": lot_size
+    }
+print(postition_calculator(capital, risk_percent, entry, stop))

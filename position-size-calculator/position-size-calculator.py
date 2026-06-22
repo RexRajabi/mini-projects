0e16d13c77d57e2c4e678a11d9723270f88e9ed9
RexRajabi <mohammad.rajabi.ba@gmail.com>
2026-06-22 14:54:12 +0330

    changed the formula
    recreated the program with a simpler formula that only
    needs the pips of stoploss and value of every pip.

--- position-size-calculator/position-size-calculator.py
+++ position-size-calculator/position-size-calculator.py
@@ -1,14 +1,14 @@
 # Position size calculator v1.0
 capital = float(input("Capital: "))
 risk_percent = float(input("Risk %: "))
-entry = float(input("Entry: "))
-stop = float(input("Stop: "))
+stoploss_depth = float(input("stop loss in pips: "))
+pip_value = float(input("pip value in your instrument: "))
 
 def postition_calculator(capital, risk_percent, entry, stop):
     risk_amount = capital * (risk_percent / 100)
-    lot_size = int(risk_amount / (entry - stop))
+    lot_size = risk_amount / (pip_value * stoploss_depth)
     return {
         "risk amount": risk_amount,
         "Lot size": lot_size
     }
-print(postition_calculator(capital, risk_percent, entry, stop))
+print(postition_calculator(capital, risk_percent, stoploss_depth, pip_value))
